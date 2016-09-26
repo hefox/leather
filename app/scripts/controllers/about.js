8e5d88204621f61fdbc486f53df84f361ed41920
Hunter Fox <hefoxed@gmail.com>
2016-09-26 16:37:03 -0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leatherApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the leatherApp
+ */
+angular.module('leatherApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leatherApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the leatherApp
+ */
+angular.module('leatherApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
